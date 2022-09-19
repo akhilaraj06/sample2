8a1ab114745880e3568a555d6f20582f0b76a4d6
91956 <akhilarajbr06@gmail.com>
2022-09-19 11:08:24 +0530

    commit

---
+++ lib/sample.dart
@@ -0,0 +1,22 @@
+// main function
+
+void main(){
+  // variable declaratrion
+  String name = "akhila";
+  int age = 22;
+  String address = "gourinandanam";
+  String mailid ="akhila@06";
+  //string interpolation
+  print(name);
+  print(age);
+  print(address);
+  print(mailid);
+
+
+
+
+
+}
+
+//void->return type
+//main->function name
